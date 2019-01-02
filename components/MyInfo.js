6885932cf09aa2a2ff86e4ba8757eaa0e76ee649
Korigan Payne <Koriganp@gmail.com>
2019-01-02 11:55:10 -0700

    added second component

---
+++ components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+	return (
+		<div>
+			<h1>Korigan Payne</h1>
+			<p>I am currently learning how to write with React. This is exciting!!</p>
+			<ul>
+				<li>Learn React</li>
+				<li>Learn Meteor</li>
+				<li>Build Web App</li>
+			</ul>
+		</div>
+	)
+}
+
+export default MyInfo
