f67847d10a2beb1e0f75493084d637eeaccf2a99
Korigan Payne <Koriganp@gmail.com>
2019-01-03 11:34:51 -0700

    made contact objects

--- components/MainContent.js
+++ components/MainContent.js
@@ -3,13 +3,45 @@ import React from "react"
 import MyInfo from "./MyInfo"
 import MyList from "./MyList"
 import Greeting from "./Greeting"
+import ContactCard from "./ContactCard"
 
 function MainContent() {
 	return (
 		<main>
+
 			<Greeting />
+
 			<MyInfo />
+
 			<MyList />
+
+			<ContactCard
+				contact={{name: "Mr. Whiskerson",
+							imgUrl: "http://placekitten.com/300/200",
+							phone: "(212) 555-1234",
+							email: "mr.whiskaz@catnap.meow"}}
+			/>
+
+			<ContactCard
+				contact={{name:"Fluffykins",
+							imgUrl:"http://placekitten.com/400/200",
+							phone:"(212) 555-2345",
+							email:"fluff@me.com"}}
+			/>
+
+			<ContactCard
+				contact={{name:"Destroyer",
+							imgUrl:"http://placekitten.com/400/300",
+							phone:"(212) 555-3456",
+							email:"ofworlds@yahoo.com"}}
+			/>
+
+			<ContactCard
+				contact={{name:"Felix",
+							imgUrl:"http://placekitten.com/200/100",
+							phone:"(212) 555-4567",
+							email:"thecat@hotmail.com"}}
+			/>
 		</main>
 	)
 }
