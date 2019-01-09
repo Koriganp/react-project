9be5b9a31714f08807b4b1b2ba81a9e84d00d338
Korigan Payne <Koriganp@gmail.com>
2019-01-09 16:57:24 -0700

    made App class

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import React from "react"
+import React, {Component} from "react"
 
 //Import my main components
 import MainContent from "./components/MainContent"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<MainContent />
-			<Footer />
-		</div>
-	)
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<div>
+				<Header />
+				<MainContent />
+				<Footer />
+			</div>
+		)
+	}
 }
 
 export default App
