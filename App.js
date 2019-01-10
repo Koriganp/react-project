38a283e9eb66b86377c07e3daa0e439d9a2ffc44
Korigan Payne <Koriganp@gmail.com>
2019-01-09 17:27:04 -0700

    practicing with componentDidMount

--- App.js
+++ App.js
@@ -4,11 +4,22 @@ import React, {Component} from "react"
 import MainContent from "./components/MainContent"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Conditional from "./components/Conditional"
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {}
+		this.state = {
+			isLoading: true
+		}
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				isLoading: false
+			})
+		}, 1500)
 	}
 
 	render() {
@@ -16,6 +27,9 @@ class App extends Component {
 			<div>
 				<Header />
 				<MainContent />
+				{this.state.isLoading ?
+					<h1>Loading...</h1> :
+					<Conditional />}
 				<Footer />
 			</div>
 		)
